9b52239b15b7e95090fdfd3d2249f336693819ff
AtreyeePhukan <atreyeephukan020104@gmail.com>
2025-10-08 14:32:34 +0530

    Fixed backend import paths for Render deployment

--- backend/predict_supervised.py
+++ backend/predict_supervised.py
@@ -18,38 +18,3 @@ def predict_log_vectorized(df):
     X_vect = vectorizer.transform(X_text)
     predictions = model.predict(X_vect)
     return predictions
-
-
-
-
-
-# import joblib
-
-# # Load model bundle
-# bundle = joblib.load("models/supervised_tfidf_model_bundle.pkl")
-# model = bundle["model"]
-# vectorizer = bundle["vectorizer"]
-
-# # def predict_log_vectorized(df):
-
-# #     X_text = (df['Log comp'] + " " + df['Log subtype']).values.astype('U')
-# #     X_vect = vectorizer.transform(X_text)
-    
-# #     predictions = model.predict(X_vect)
-# #     return predictions
-
-
-# def predict_log_vectorized(df):
-#     X_text = (
-#         df['Action'].astype(str) + " " +
-#         df['Protocol'].astype(str) + " " +
-#         df['Source_IP'].astype(str) + " " +
-#         df['Destination_IP'].astype(str) + " " +
-#         df['Source_Port'].astype(str) + " " +
-#         df['Destination_Port'].astype(str) + " " +
-#         df['Packet_Size'].astype(str)
-#     ).values.astype('U')
-
-#     X_vect = vectorizer.transform(X_text)
-#     predictions = model.predict(X_vect)
-#     return predictions
