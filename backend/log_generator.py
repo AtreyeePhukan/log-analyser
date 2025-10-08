92fbbc00a390e2551f88e12faca605412605a101
AtreyeePhukan <atreyeephukan020104@gmail.com>
2025-10-08 15:00:24 +0530

    Fix log generator import for deployment

--- backend/log_generator.py
+++ backend/log_generator.py
@@ -1,6 +1,6 @@
 import random
 from datetime import datetime, timezone
-from log_nlp import process_log_entry
+from backend.log_nlp import process_log_entry
 import asyncio
 
 sample_ips = ["10.0.0.1", "10.0.0.2", "192.168.1.3", "172.16.0.5"]
